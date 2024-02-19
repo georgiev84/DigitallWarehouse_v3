05ba7bba8dedd42f15be3475906de74dfb191d96
Petar Georgiev <P.G.Georgiev@digitall.com>
2024-02-19 11:59:28 +0200

    Fix Empty rows in models in API

--- Warehouse.Api/Models/Requests/Product/ProductUpdateRequest.cs
+++ Warehouse.Api/Models/Requests/Product/ProductUpdateRequest.cs
@@ -4,13 +4,9 @@ public class ProductUpdateRequest
 {
     public Guid Id { get; set; }
     public Guid BrandId { get; set; }
-
     public string Title { get; set; }
-
     public string Description { get; set; }
-
     public decimal Price { get; set; }
-
     public List<Guid> GroupIds { get; set; }
     public List<SizeInformationRequest> SizeInformation { get; set; }
 }
